76ef2c55775aa65fb7dfc826f482367b3b9266b1
iaroslav74 <9169671422@mail.ru>
2022-04-18 22:38:31 +0300

    Initial commit

---
+++ src/main/java/Domaska7/Main.java
@@ -0,0 +1,9 @@
+package Domaska7;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
---
+++ src/main/java/Domaska7/Period.java
@@ -0,0 +1,5 @@
+package Domaska7;
+
+public enum Period {
+    NOW, FIVE_DAYS, DB
+}
